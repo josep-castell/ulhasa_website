49a0625cead268e9e5b67c702518282959a312ac
Josep <jcastellcolom@cifpfbmoll.eu>
2022-02-22 22:08:52 +0100

    Canvi animacio

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const landingImage = document.querySelector(".landing-img");
+const landingBackground = document.querySelector(".landing-img-container");
 function landingImgLoad() {
   landingImage.style.opacity = ".9";
+  landingBackground.style.backgroundColor = "rgba(255,255,255,0)";
 }
