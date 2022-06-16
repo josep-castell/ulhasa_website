ce044e7059821dd36afa230a8a13a5d6401496c8
Josep <jcastellcolom@cifpfbmoll.eu>
2022-06-16 12:18:13 +0200

    working on: new background

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-const landingImage = document.querySelector(".landing-img");
-const landingBackground = document.querySelector(".landing-img-container");
-function landingImgLoad() {
-  landingImage.style.opacity = ".9";
-  landingBackground.style.backgroundColor = "rgba(255,255,255,0)";
-}
+const options = {
+  root: document.querySelector(".landing-img"),
+  rootMargin: "0px",
+  threshold: 0,
+};
+
+let callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+  });
+};
+
+let observer = new IntersectionObserver(callback, options);
+
+let target = document.getElementById("whoAmI");
+
+observer.observe(target);
